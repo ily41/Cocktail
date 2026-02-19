ce86cf52fd42cf30770c0aef22286e1a340a62ab
ily41 <raufilyayi@gmail.com>
2026-02-19 23:48:55 +0400

    implementing the art section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -70,7 +70,7 @@ const Hero = () => {
 
         videoRef.current.onloadedmetadata = () => {
             tl.to(videoRef.current, {
-                currentTime: videoRef.current.duration
+                currentTime: videoRef.current?.duration
             })
         }
     }, [])
